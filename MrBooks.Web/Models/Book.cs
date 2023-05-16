7fd7a4f26c00c75b658a4d1c173399789f615637
angelstchavez <angelstchavez@gmail.com>
2023-05-15 19:52:19 -0500

    Agregue la clase Book

---
+++ MrBooks.Web/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace MrBooks.Web.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string? Name { get; set; } = string.Empty;
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
