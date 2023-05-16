ed5730e966fa7186f587efa74d1c885832140743
angelstchavez <angelstchavez@gmail.com>
2023-05-15 20:08:38 -0500

    Agregue la clase ConnectionFactory

---
+++ MrBooks.Web/Data/ConnectionFactory.cs
@@ -0,0 +1,31 @@
+﻿namespace MrBooks.Web.Data
+{
+    /// <summary>
+    /// Clase que representa una fábrica de conexiones.
+    /// </summary>
+    public class ConnectionFactory
+    {
+        private string ConnectionString = string.Empty;
+
+        /// <summary>
+        /// Constructor de la clase ConnectionFactory.
+        /// </summary>
+        public ConnectionFactory()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+
+            ConnectionString = builder.GetSection("ConnectionString:ConnectionStringSQL").Value;
+        }
+
+        /// <summary>
+        /// Obtiene la cadena de conexión.
+        /// </summary>
+        /// <returns>
+        /// La cadena de conexión para acceder a la base de datos.
+        /// </returns>
+        public string GetConnectionString()
+        {
+            return ConnectionString;
+        }
+    }
+}
