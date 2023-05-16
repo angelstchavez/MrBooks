934d3c836f7d70bb4e4be42d3f11f32c66648db0
angelstchavez <angelstchavez@gmail.com>
2023-05-15 20:20:28 -0500

    Agregue la interface IBookRepository

---
+++ MrBooks.Web/Interface/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using MrBooks.Web.Models;
+
+namespace MrBooks.Web.Interface
+{
+    public interface IBookRepository
+    {
+        List<Book> GetBooks();
+        bool Save(Book book);
+        bool Update(Book book);
+        bool Delete(int id);
+        Book GetById(int id);
+    }
+}
